fbe9098330a3d4b208acc03200dbcf858e14b755
Grab <apple@MacBook-Pro-3.local>
2020-12-14 23:31:27 +0900

    메인 화면 - 배너 추가 & 버그 수정

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  Alert,
+  SafeAreaView,
+} from "react-native";
 import BasketballImage from "./assets/products/basketball1.jpeg";
 import Avatar from "./assets/icons/avatar.png";
 import { API_URL } from "./config/constants";
@@ -8,11 +18,13 @@ import axios from "axios";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
+import Carousel from "react-native-snap-carousel";
 dayjs.extend(relativeTime);
 dayjs.locale("ko");
 
 export default function App() {
   const [products, setProducts] = React.useState([]);
+  const [banners, setBanners] = React.useState([]);
   React.useEffect(() => {
     axios
       .get(`${API_URL}/products`)
@@ -22,45 +34,77 @@ export default function App() {
       .catch((error) => {
         console.error(error);
       });
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        setBanners(result.data.banners);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <Text style={styles.headline}>판매되는 상품들</Text>
-        <View style={styles.productList}>
-          {products.map((product, index) => {
-            return (
-              <View style={styles.productCard}>
-                <View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.container}>
+        <ScrollView>
+          <Carousel
+            data={banners}
+            sliderWidth={Dimensions.get("window").width}
+            itemWidth={Dimensions.get("window").width}
+            itemHeight={200}
+            renderItem={(obj) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    Alert.alert("배너 클릭");
+                  }}
+                >
                   <Image
-                    style={styles.productImage}
-                    source={{
-                      uri: `${API_URL}/${product.imageUrl}`,
-                    }}
-                    resizeMode={"contain"}
+                    style={styles.bannerImage}
+                    source={{ uri: `${API_URL}/${obj.item.imageUrl}` }}
+                    resizeMode="contain"
                   />
-                </View>
-                <View style={styles.productContents}>
-                  <Text style={styles.productName}>{product.name}</Text>
-                  <Text style={styles.productPrice}>{product.price}원</Text>
-                  <View style={styles.productFooter}>
-                    <View style={styles.productSeller}>
-                      <Image style={styles.productAvatar} source={Avatar} />
-                      <Text style={styles.productSellerName}>
-                        {product.seller}
+                </TouchableOpacity>
+              );
+            }}
+          />
+          <Text style={styles.headline}>판매되는 상품들</Text>
+          <View style={styles.productList}>
+            {products.map((product, index) => {
+              return (
+                <View style={styles.productCard} key={index}>
+                  {product.soldout === 1 && <View style={styles.productBlur} />}
+                  <View>
+                    <Image
+                      style={styles.productImage}
+                      source={{
+                        uri: `${API_URL}/${product.imageUrl}`,
+                      }}
+                      resizeMode={"contain"}
+                    />
+                  </View>
+                  <View style={styles.productContents}>
+                    <Text style={styles.productName}>{product.name}</Text>
+                    <Text style={styles.productPrice}>{product.price}원</Text>
+                    <View style={styles.productFooter}>
+                      <View style={styles.productSeller}>
+                        <Image style={styles.productAvatar} source={Avatar} />
+                        <Text style={styles.productSellerName}>
+                          {product.seller}
+                        </Text>
+                      </View>
+                      <Text style={styles.productDate}>
+                        {dayjs(product.createdAt).fromNow()}
                       </Text>
                     </View>
-                    <Text style={styles.productDate}>
-                      {dayjs(product.createdAt).fromNow()}
-                    </Text>
                   </View>
                 </View>
-              </View>
-            );
-          })}
-        </View>
-      </ScrollView>
-    </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -68,7 +112,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 32,
   },
   productCard: {
     width: 300,
@@ -120,5 +163,23 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "800",
     marginBottom: 24,
+    marginLeft: 12,
+  },
+  productBlur: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: "#ffffffaa",
+    zIndex: 999,
+  },
+  bannerImage: {
+    width: "100%",
+    height: 200,
+  },
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: "#fff",
   },
 });
