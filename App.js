a634daea7af0478cbe1280a60c12fe59bed625f5
Grab <apple@MacBook-Pro-3.local>
2020-12-14 20:33:15 +0900

    메인 화면 - 상품 카드 구현

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import BasketballImage from "./assets/products/basketball1.jpeg";
+import Avatar from "./assets/icons/avatar.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>판매되는 상품들</Text>
+      <View style={styles.productCard}>
+        <View>
+          <Image
+            style={styles.productImage}
+            source={BasketballImage}
+            resizeMode={"contain"}
+          />
+        </View>
+        <View style={styles.productContents}>
+          <Text style={styles.productName}>농구공</Text>
+          <Text style={styles.productPrice}>50000원</Text>
+          <View style={styles.productFooter}>
+            <View style={styles.productSeller}>
+              <Image style={styles.productAvatar} source={Avatar} />
+              <Text style={styles.productSellerName}>그랩</Text>
+            </View>
+            <Text style={styles.productDate}>3분 전</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +35,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 32,
+  },
+  productCard: {
+    width: 300,
+    borderColor: "rgb(230,230,230)",
+    borderWidth: 1,
+    borderRadius: 16,
+    backgroundColor: "white",
+  },
+  productImage: {
+    width: "100%",
+    height: 210,
+  },
+  productContents: {
+    padding: 8,
+  },
+  productSeller: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  productAvatar: {
+    width: 24,
+    height: 24,
+  },
+  productFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 12,
+  },
+  productName: {
+    fontSize: 16,
+  },
+  productPrice: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 8,
+  },
+  productSellerName: {
+    fontSize: 16,
+  },
+  productDate: {
+    fontSize: 16,
   },
 });
