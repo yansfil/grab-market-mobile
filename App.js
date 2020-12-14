501ff299c794a83bd55aff0fa19c62dce9b535f0
Grab <apple@MacBook-Pro-3.local>
2020-12-14 20:34:42 +0900

    메인 화면 - 상품 리스트 구현

--- App.js
+++ App.js
@@ -1,33 +1,65 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import BasketballImage from "./assets/products/basketball1.jpeg";
 import Avatar from "./assets/icons/avatar.png";
+import { API_URL } from "./config/constants";
+import axios from "axios";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+dayjs.extend(relativeTime);
+dayjs.locale("ko");
 
 export default function App() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(() => {
+    axios
+      .get(`${API_URL}/products`)
+      .then((result) => {
+        setProducts(result.data.products);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>판매되는 상품들</Text>
-      <View style={styles.productCard}>
-        <View>
-          <Image
-            style={styles.productImage}
-            source={BasketballImage}
-            resizeMode={"contain"}
-          />
+      <ScrollView>
+        <Text style={styles.headline}>판매되는 상품들</Text>
+        <View style={styles.productList}>
+          {products.map((product, index) => {
+            return (
+              <View style={styles.productCard}>
+                <View>
+                  <Image
+                    style={styles.productImage}
+                    source={{
+                      uri: `${API_URL}/${product.imageUrl}`,
+                    }}
+                    resizeMode={"contain"}
+                  />
+                </View>
+                <View style={styles.productContents}>
+                  <Text style={styles.productName}>{product.name}</Text>
+                  <Text style={styles.productPrice}>{product.price}원</Text>
+                  <View style={styles.productFooter}>
+                    <View style={styles.productSeller}>
+                      <Image style={styles.productAvatar} source={Avatar} />
+                      <Text style={styles.productSellerName}>
+                        {product.seller}
+                      </Text>
+                    </View>
+                    <Text style={styles.productDate}>
+                      {dayjs(product.createdAt).fromNow()}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            );
+          })}
         </View>
-        <View style={styles.productContents}>
-          <Text style={styles.productName}>농구공</Text>
-          <Text style={styles.productPrice}>50000원</Text>
-          <View style={styles.productFooter}>
-            <View style={styles.productSeller}>
-              <Image style={styles.productAvatar} source={Avatar} />
-              <Text style={styles.productSellerName}>그랩</Text>
-            </View>
-            <Text style={styles.productDate}>3분 전</Text>
-          </View>
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -44,6 +76,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 16,
     backgroundColor: "white",
+    marginBottom: 8,
   },
   productImage: {
     width: "100%",
@@ -79,5 +112,13 @@ const styles = StyleSheet.create({
   },
   productDate: {
     fontSize: 16,
+  },
+  productList: {
+    alignItems: "center",
+  },
+  headline: {
+    fontSize: 24,
+    fontWeight: "800",
+    marginBottom: 24,
   },
 });
---
+++ config/constants.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://6203c5b24837.ngrok.io";
